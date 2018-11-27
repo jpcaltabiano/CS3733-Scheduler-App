932e9d981e300093f68b90ad26768c4c24ef7f00
Wai Khumwang <43976853+wai311040@users.noreply.github.com>
2018-11-27 13:56:15 -0500

    lambda setup

--- src/main/java/zosma/controller/CreateScheduleHandler.java
+++ src/main/java/zosma/controller/CreateScheduleHandler.java
@@ -1,9 +1,16 @@
 package zosma.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
@@ -40,7 +47,73 @@ public class CreateScheduleHandler implements RequestStreamHandler  {
 
 	@Override
 	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-		// TODO Auto-generated method stub
+		/*logger = context.getLogger();
+		logger.log("Loading Java Lambda handler to create constant");
 
+		JSONObject headerJson = new JSONObject();
+		headerJson.put("Content-Type",  "application/json");  // not sure if needed anymore?
+		headerJson.put("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+	    headerJson.put("Access-Control-Allow-Origin",  "*");
+	        
+		JSONObject responseJson = new JSONObject();
+		responseJson.put("headers", headerJson);
+
+		CreateConstantResponse response = null;
+		
+		// extract body from incoming HTTP POST request. If any error, then return 422 error
+		String body;
+		boolean processed = false;
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+			JSONParser parser = new JSONParser();
+			JSONObject event = (JSONObject) parser.parse(reader);
+			logger.log("event:" + event.toJSONString());
+			
+			String method = (String) event.get("httpMethod");
+			if (method != null && method.equalsIgnoreCase("OPTIONS")) {
+				logger.log("Options request");
+				response = new CreateConstantResponse("name", 200);  // OPTIONS needs a 200 response
+		        responseJson.put("body", new Gson().toJson(response));
+		        processed = true;
+		        body = null;
+			} else {
+				body = (String)event.get("body");
+				if (body == null) {
+					body = event.toJSONString();  // this is only here to make testing easier
+				}
+			}
+		} catch (ParseException pe) {
+			logger.log(pe.toString());
+			response = new CreateConstantResponse("Bad Request:" + pe.getMessage(), 422);  // unable to process input
+	        responseJson.put("body", new Gson().toJson(response));
+	        processed = true;
+	        body = null;
+		}
+
+		if (!processed) {
+			CreateConstantRequest req = new Gson().fromJson(body, CreateConstantRequest.class);
+			logger.log(req.toString());
+
+			CreateConstantResponse resp;
+			try {
+				if (createConstant(req.name, req.value)) {
+					resp = new CreateConstantResponse("Successfully defined constant:" + req.name);
+				} else {
+					resp = new CreateConstantResponse("Unable to create constant: " + req.name, 422);
+				}
+			} catch (Exception e) {
+				resp = new CreateConstantResponse("Unable to create constant: " + req.name + "(" + e.getMessage() + ")", 403);
+			}
+
+			// compute proper response
+	        responseJson.put("body", new Gson().toJson(resp));  
+		}
+		
+        logger.log("end result:" + responseJson.toJSONString());
+        logger.log(responseJson.toJSONString());
+        OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8");
+        writer.write(responseJson.toJSONString());  
+        writer.close();
+*/
 	}
 }
