ac7cc79512c761cf4822a1b7fdfc674f26d080e4
yqiu3 <1325126057@qq.com>
2018-12-10 11:33:42 -0500

    Add files via upload

---
+++ UI/error/js/index.js
@@ -0,0 +1,10 @@
+window.onload = function (){  
+		var bigImg = document.createElement("img");		
+		bigImg.src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2016/04/1496946239error.jpg";  
+		var myDiv = document.getElementById('myDiv'); 
+		myDiv.appendChild(bigImg); 
+	};
+
+function tz(){
+    window.location.href='black.html';
+}
